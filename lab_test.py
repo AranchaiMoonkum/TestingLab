4a60c842b4803e88a28f727c4f5f2ba2fed5ee21
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2024-12-08 06:58:09 +0900

    Added lab_test.py

---
+++ lab_test.py
@@ -0,0 +1,11 @@
+import unittest
+import lab
+
+class TestLab(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(lab.calculate_age(2004), 20)
+    def test_case_2(self):
+        self.assertEqual(lab.calculate_grade(95), "A")
+
+if __name__ == "__main__":
+    unittest.main()
