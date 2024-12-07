6329350588195575cfc74378424dec9f5756e858
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2024-12-08 06:57:47 +0900

    Updated lab.py

--- lab.py
+++ lab.py
@@ -3,30 +3,30 @@ input_name = input("Name: ")
 input_age = int(input("Christian Era: "))
 input_score = int(input("Software testing score: "))
 
-# Convert the data
-year = 2024
-age = year - input_age
+def calculate_age(input_age):
+    year = 2024
+    return year - input_age
 
-grade = ""
-if input_score >= 80:
-    grade = "A"
-elif input_score >= 75:
-    grade = "B+"
-elif input_score >= 70:
-    grade = "B"
-elif input_score >= 65:
-    grade = "C+"
-elif input_score >= 60:
-    grade = "C"
-elif input_score >= 55:
-    grade = "D+"
-elif input_score >= 50:
-    grade = "D"
-elif input_score < 50:
-    grade = "F"
+def calculate_grade(input_score):
+    if input_score >= 80:
+        return "A"
+    elif input_score >= 75:
+        return "B+"
+    elif input_score >= 70:
+        return "B"
+    elif input_score >= 65:
+        return "C+"
+    elif input_score >= 60:
+        return "C"
+    elif input_score >= 55:
+        return "D+"
+    elif input_score >= 50:
+        return "D"
+    elif input_score < 50:
+        return "F"
 
 # Write the data to a file
 with open("lab.txt", "w") as file:
     file.write(f"Name: {input_name}\n")
-    file.write(f"Age: {age}\n")
-    file.write(f"Software testing grade: {grade}\n")
+    file.write(f"Age: {calculate_age(input_age)}\n")
+    file.write(f"Software testing grade: {calculate_grade(input_score)}\n")
