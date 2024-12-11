8913293fd39b4d9806b732b1798e7c79addf8a41
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2024-12-12 00:39:59 +0700

    Updated lab.py

--- lab.py
+++ lab.py
@@ -1,32 +1,58 @@
 import datetime
 
-input_name = input("Name: ")
-input_age = int(input("Christian Era: "))
-input_score = int(input("Software testing score: "))
+def get_input():
+    input_name = input("Enter your name: ")
 
-def calculate_age(input_age):
+    try:
+        input_age = int(input("Enter year of birth in christian era: "))
+        input_score = int(input("Software testing score: "))
+    except ValueError:
+        print("Invalid input")
+        exit()
+
+    return input_name, input_age, input_score
+
+def calculate_age(year):
     current_year = datetime.datetime.now().year
-    return current_year - input_age
+    age = current_year - year
 
-def calculate_grade(input_score):
-    if input_score >= 80:
+    return age
+
+def calculate_grade(score):
+    if not (0 <= score <= 100):
+        return "Invalid score"
+
+    if score >= 80:
         return "A"
-    elif input_score >= 75:
+    elif score >= 75:
         return "B+"
-    elif input_score >= 70:
+    elif score >= 70:
         return "B"
-    elif input_score >= 65:
+    elif score >= 65:
         return "C+"
-    elif input_score >= 60:
+    elif score >= 60:
         return "C"
-    elif input_score >= 55:
+    elif score >= 55:
         return "D+"
-    elif input_score >= 50:
+    elif score >= 50:
         return "D"
-    elif input_score < 50:
+    else:
         return "F"
 
-with open("lab.txt", "w") as file:
-    file.write(f"Name: {input_name}\n")
-    file.write(f"Age: {calculate_age(input_age)}\n")
-    file.write(f"Software testing grade: {calculate_grade(input_score)}\n")
+def write_to_file(input_name, year_of_birth, score):
+    file_path = "lab.txt"
+
+    with open(file_path, "w") as file:
+        file.write(f"Name: {input_name} \n")
+        file.write(f"Age: {calculate_age(year_of_birth)} \n")
+        file.write(f"Grade: {calculate_grade(score)} \n")
+
+    print(f"Data was written to the file path: {file_path}")
+
+def main():
+    input_name, input_age, input_score = get_input()
+
+    write_to_file(input_name, input_age, input_score)
+
+if __name__ == "__main__":
+    main()
